47f43947837d8fcd131ffad0956dc01529a481e7
QdotD <Quinton.digital@protonmail.com>
2022-08-26 18:07:22 -0400

    connected sanity client to app and started passing in data from sanity

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,48 @@
 // rafce
+// imports react to the project
 import React from 'react'
 
-import {Product, FooterBanner, HeroBanner } from '../components';
+// imports sanityClient to the project
+import { client } from '../lib/client';
 
-const Home = () => {
-  return (
-    <>
-        <HeroBanner />
+//imports react components to the project
+import { Product, FooterBanner, HeroBanner } from '../components';
 
-        <div className="products-heading">
-            <h2>Best Selling Products</h2>
-            <p>Many types of speakers</p>
-        </div>
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner />
 
-        <div className="products-container">
-            {['Product 1', 'Product 2'].map(
-                (product) => product)}
-        </div>
+    {console.log(bannerData)}
 
-        <FooterBanner />
-    </>
-  )
+    <div className="products-heading">
+      <h2>Best Selling Products</h2>
+      <p>Many types of speakers</p>
+    </div>
+
+    <div className="products-container">
+      {/* question mark makes sure products exist before executing .map */}
+      {products?.map((product) => product.name)}
+    </div>
+
+    <FooterBanner />
+  </div>
+);
+
+
+
+// async function that allows us to fetch data from APIs ("Next.js with pre-render this page on each request using the data returned by getServerSideProps")
+export const getServerSideProps = async () => {
+  // lets grab all our products from the sanity dashboard
+  const query  = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  // grabs all banner data
+  const bannerQuery  = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
-export default Home
+export default Home;
