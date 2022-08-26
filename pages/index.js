d8732c35156c95c28f6c44428bd190de939fc258
QdotD <Quinton.digital@protonmail.com>
2022-08-26 16:43:26 -0400

    created index.js and removed unneeded styles file

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
+// rafce
 import React from 'react'
 
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+        HeroBanner
+
+        <div>
+            <h2>Best Selling Products</h2>
+            <p>Many types of speakers</p>
+        </div>
+
+        <div>
+            {['Product 1', 'Product 2'].map(
+                (product) => product)}
+        </div>
+
+        Footer
+    </>
   )
 }
 
-export default index
+export default Home
