4dd7fb943fe330a4821975170d0e3098357494ca
QdotD <quinton.digital@protonmail.com>
2022-09-18 19:17:53 -0400

    setup Layout.jsx component and prop drilled the other components into it

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { Layout } from '../components';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // "Component" is a dynamic component based on which page user is on
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
