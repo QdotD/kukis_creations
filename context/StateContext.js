017da81a224f81812039c9481fe08d7057a6813b
QdotD <quinton.digital@protonmail.com>
2022-09-20 15:34:40 -0400

    implemented state context in the navbar

--- context/StateContext.js
+++ context/StateContext.js
@@ -8,7 +8,7 @@ export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState();
+    const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
     // state functions
@@ -66,6 +66,7 @@ export const StateContext = ({ children }) => {
                 incQty,
                 decQty,
                 onAdd,
+                setShowCart,
             }}
         >
             {children}
