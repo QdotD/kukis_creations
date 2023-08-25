1fd1012c6128e09c92d1a0805fa54cd42ad23459
QdotD <quinton.digital@protonmail.com>
2023-08-24 23:36:13 -0700

    logos working?

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import NoSsr from './NoSsr';
 
+
+
 const Footer = () => {
   return (
     <div className="footer-container">
       <p className="icons">
         <NoSsr>
           <a href="https://www.instagram.com/kukiscreationsdotcom/" target="_blank" rel="noopener noreferrer">
-            <img src="/instagram.svg" alt="Instagram logo" width={30} height={30} />
+            <img src="instagram.svg" alt="Instagram logo" width={30} height={30} />
           </a>
           <a href="https://www.etsy.com/shop/KUKIsCREATIONSdotcom/" target="_blank" rel="noopener noreferrer">
-            <img src="/etsy.svg" alt="Etsy logo" width={50} height={45} />
+            <img src="etsy.svg" alt="Etsy logo" width={50} height={45} />
           </a>
         </NoSsr>
       </p>
