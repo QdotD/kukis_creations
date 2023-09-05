7e4684d415046b0766d1afe0383348f1ab053d69
QdotD <quinton.digital@protonmail.com>
2023-09-04 20:53:34 -0700

    fixed about page &apos; error

--- pages/about.jsx
+++ pages/about.jsx
@@ -9,10 +9,10 @@ const AboutPage = () => {
         
         <div className="about-body-text">
 
-            <h6>Hello, I'm Kuki, the artist behind the scenes, and I'm thrilled to take you on a journey through my world of creativity!</h6>
+            <h6>Hello, I&apos;m Kuki, the artist behind the scenes, and I&apos;m thrilled to take you on a journey through my world of creativity!</h6>
             <br /><br />
     
-            <p>Ever since I could hold a pencil, all I wanted to do was scribble, color, and draw "cool" things to show my mom, 
+            <p>Ever since I could hold a pencil, all I wanted to do was scribble, color, and draw &quot;cool&quot; things to show my mom, 
             who always supported me despite the green suns, blue-faced mermaids, and disfigured shapes that I initially drew. 
             I can proudly say that I have improved since then, thankfully, and my passion for art has only grown with time. 
             I have dabbled in different media and explored various forms of art over many years and learned a lot. However, 
@@ -21,7 +21,7 @@ const AboutPage = () => {
             <br /><br />
             In my store, you will find a wide range of products, including 3D printed art (which I began when I was in school 
             for engineering), jewelry, sculptures, keychains, and many more to come (I do hope to incorporate electronics into
-            my art very soon). I also enjoy creating art inspired by things I'm passionate about, such as video games, Studio Ghibli, 
+            my art very soon). I also enjoy creating art inspired by things I&apos;m passionate about, such as video games, Studio Ghibli, 
             Disney-related themes, and more. Despite the various media I work with and the diverse inspirations and themes I base my art 
             on, they all share one common element - they are made with love and care (yes, it may sound cliché, but I genuinely mean it). 
             Each piece involves hours, if not days, of painstaking work, ensuring that no two pieces are 100% alike or mass-produced. 
@@ -29,11 +29,11 @@ const AboutPage = () => {
             that will last, allowing you to enjoy your piece of art for as long as possible. 
             <br /><br />
             To stay connected with me on this creative journey, please follow the store page on Instagram (@kukiscreationsdotcom). 
-            It's a place where I share sneak peeks of upcoming projects and offer behind-the-scenes glimpses into my creative process. 
-            If you have any questions or would like to inquire about a custom piece, please don't hesitate to reach out to me via the 
+            It&apos;s a place where I share sneak peeks of upcoming projects and offer behind-the-scenes glimpses into my creative process. 
+            If you have any questions or would like to inquire about a custom piece, please don&apos;t hesitate to reach out to me via the 
             commissions page, Instagram DMs, or email (kukiscreations.proton@me).
             <br /><br />
-            Thank you for visiting Kuki’s Creations. I hope my art brings a touch of magic and wonder into your life :)
+            Thank you for visiting Kuki&apos;s Creations. I hope my art brings a touch of magic and wonder into your life :)
             <br /><br />
             </p>
 
