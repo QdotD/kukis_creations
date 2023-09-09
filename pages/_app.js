105376e5b03880a851ae97fb0ff75e84789a9080
QdotD <quinton.digital@protonmail.com>
2023-09-09 00:58:01 -0700

    updated gtm tag

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ function MyApp({ Component, pageProps }) {
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://bremqgve.lusw.stape.io/bremqgve.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
       `}
         </Script>
